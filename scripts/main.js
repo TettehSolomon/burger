41b86273ffc41550e1de13cc13ac843bc4fd7897
TettehSolomon <tettehsolomon106@gmail.com>
2024-04-20 12:37:40 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
+// Variable declarations
 let iconCart = document.querySelector('.icon-cart');
-let closeCart = document.querySelector('.close')
+let closeCart = document.querySelector('.close');
 let body = document.querySelector('body');
 let listProductHTML = document.querySelector('.listProduct');
 let listCartHTML = document.querySelector('.listCart');
@@ -8,6 +9,7 @@ let iconCartSpan = document.querySelector('.icon-cart span');
 let listProducts = [];
 let carts = [];
 
+// Event listeners
 iconCart.addEventListener('click', () => {
     body.classList.toggle('showCart');
 });
@@ -16,9 +18,10 @@ closeCart.addEventListener('click', () => {
     body.classList.toggle('showCart');
 });
 
+// Functions
 const addDataToHtml = () => {
     listProductHTML.innerHTML = '';
-    if(listProducts.length > 0){
+    if (listProducts.length > 0) {
         listProducts.forEach(product => {
             let newProduct = document.createElement('div');
             newProduct.classList.add('item');
@@ -30,9 +33,9 @@ const addDataToHtml = () => {
                 <button class="addCart">Add To Cart</button>
             `;
             listProductHTML.appendChild(newProduct);
-        })
+        });
     }
-}
+};
 
 listProductHTML.addEventListener('click', (event) => {
     let positionClick = event.target;
@@ -44,31 +47,30 @@ listProductHTML.addEventListener('click', (event) => {
 
 const addToCart = (product_id) => {
     let positionThisProductInCart = carts.findIndex((value) => value.product_id == product_id);
-    if(carts.length <= 0){
+    if (carts.length <= 0) {
         carts = [{
             product_id: product_id,
             quantity: 1
-        }]
-    }else if(positionThisProductInCart < 0){
+        }];
+    } else if (positionThisProductInCart < 0) {
         carts.push({
             product_id: product_id,
             quantity: 1
         });
-    }else{
+    } else {
         carts[positionThisProductInCart].quantity = carts[positionThisProductInCart].quantity + 1;
     }
-    addCartToHTML();
-    addCartToMemory();
-}
+    updateCartAndTotal();
+};
 
 const addCartToMemory = () => {
     localStorage.setItem('cart', JSON.stringify(carts));
-}
+};
 
 const addCartToHTML = () => {
     listCartHTML.innerHTML = '';
     let totalQuantity = 0;
-    if(carts.length > 0){
+    if (carts.length > 0) {
         carts.forEach(cart => {
             totalQuantity = totalQuantity + cart.quantity;
             let newCart = document.createElement('div');
@@ -88,10 +90,10 @@ const addCartToHTML = () => {
                     <span class="plus">+</span>
                 </div>`;
             listCartHTML.appendChild(newCart);
-        })
+        });
     }
     iconCartSpan.innerText = totalQuantity;
-}
+};
 
 listCartHTML.addEventListener('click', (event) => {
     let positionClick = event.target;
@@ -107,73 +109,66 @@ listCartHTML.addEventListener('click', (event) => {
 
 const changeQuantity = (product_id, type) => {
     let positionItemInCart = carts.findIndex((value) => value.product_id == product_id);
-    if(positionItemInCart >= 0){
+    if (positionItemInCart >= 0) {
         switch (type) {
             case 'plus':
                 carts[positionItemInCart].quantity = carts[positionItemInCart].quantity + 1;
                 break;
-
             default:
                 let valueChange = carts[positionItemInCart].quantity - 1;
-                if(valueChange > 0){
+                if (valueChange > 0) {
                     carts[positionItemInCart].quantity = valueChange;
-                }else{
+                } else {
                     carts.splice(positionItemInCart, 1);
                 }
                 break;
         }
     }
-    addCartToMemory();
-    addCartToHTML();
-}
+    updateCartAndTotal();
+};
 
 const initApp = () => {
-    //get data from json
+    // Fetch data from json
     fetch('products.json')
     .then(response => response.json())    
     .then(data => {
         listProducts = data;
         addDataToHtml();
 
-        //get cart from memory
-        if(localStorage.getItem('cart')){
+        // Retrieve cart from memory
+        if (localStorage.getItem('cart')) {
             carts = JSON.parse(localStorage.getItem('cart'));
             addCartToHTML();
         }
-    })
-}
+    });
 
+    updateTotalPrice();
+};
+
+const calculateTotalPrice = () => {
+    let totalPrice = 0;
+    carts.forEach(cart => {
+        let positionProduct = listProducts.findIndex((value) => value.id == cart.product_id);
+        if (positionProduct !== -1) {
+            let product = listProducts[positionProduct];
+            totalPrice += product.price * cart.quantity;
+        }
+    });
+    return totalPrice;
+};
+
+const updateTotalPrice = () => {
+    const totalAmountElement = document.querySelector('.total-amount');
+    if (totalAmountElement) {
+        totalAmountElement.textContent = calculateTotalPrice();
+    }
+};
+
+const updateCartAndTotal = () => {
+    addCartToMemory();
+    addCartToHTML();
+    updateTotalPrice();
+};
+
+// Call initApp to initialize the application
 initApp();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
